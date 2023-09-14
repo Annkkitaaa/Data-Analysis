1226d342cced18a66c8ae272ef8e8b356c6c058d
Ankita Singh <ankitasingh15.102@gmail.com>
2023-09-15 01:16:10 +0530

    sales_analysis.m

---
+++ EDA project-1/sales_analysis.m
@@ -0,0 +1,21 @@
+Diwali Sales Analysis EDA Project
+OVERVIEW
+Diwali, also known as the Festival of Lights, is one of the most significant festivals celebrated in India and various other countries. During this festive season, there is a significant surge in sales across various industries, including retail, electronics, fashion, and more.
+This EDA project aims to answer intriguing questions such as:
+
+What are the key factors that drive sales during Diwali?
+How do sales trends change over the years?
+Which product categories or regions experience the highest growth in sales?
+Are there any noticeable patterns in customer behavior during Diwali?
+We will explore these questions and more by analyzing a rich dataset of Diwali sales data, which includes information about products, sales transactions, customer demographics, and time-based trends.
+
+Project Structure
+This repository is organized as follows:
+
+Data: This directory contains the raw data used for the analysis. [Add details about data sources and files here]
+
+Notebooks: You'll find Jupyter notebooks that walk you through the entire data analysis process. We provide step-by-step explanations and code to help you understand our insights and methodology.
+
+Visualizations: This directory includes visualizations, charts, and graphs generated during the analysis. Visualizations are crucial for conveying insights effectively.
+
+Reports: Here, you'll find reports summarizing our findings, conclusions, and recommendations.
